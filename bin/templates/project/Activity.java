571a13539dfeaa98aa75756a1b6182781f14848f
zhangjy <jy-zhang@zakj.info>
2019-11-28 09:42:57 +0800

    Update Activity.java

--- bin/templates/project/Activity.java
+++ bin/templates/project/Activity.java
@@ -29,6 +29,23 @@ public class __ACTIVITY__ extends CordovaActivity
     {
         super.onCreate(savedInstanceState);
 
+       //华为机型开始连接华为push服务器
+        if (OSHelper.isEmui()){
+            HMSAgent.connect(this, new ConnectHandler() {
+                @Override
+                public void onConnect(int rst) {
+                    System.out.println("HMS connect end:" + rst);
+                    HMSAgent.Push.getToken(new GetTokenHandler() {
+                        @Override
+                        public void onResult(int rst) {
+                            System.out.println("get token: end" + rst);
+
+                        }
+                    });
+
+                }
+            });
+        }
         // enable Cordova apps to be started in the background
         Bundle extras = getIntent().getExtras();
         if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
